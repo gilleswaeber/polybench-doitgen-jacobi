4bbec56ad35b0255b466c288d9fad77fc97354c1
adegendt <adegendt@student.ethz.ch>
2021-11-21 15:35:35 +0100

    added papi

--- doitgen/benchmark/openMPBenchmark.cpp
+++ doitgen/benchmark/openMPBenchmark.cpp
@@ -1,6 +1,7 @@
 #include <doitgen.hpp>
 #include <utils.hpp>
 #include <liblsb.h>
+#include <papi.h>
 
 #define RUNS 10
 
@@ -24,14 +25,33 @@ int main() {
 	double* c4;
 	double* sum;
 	init(&a_in, &a_out, &c4, &sum, nr, nq, np);
-	MPI::Init();
 
-	LSB_Init("doitgen-openMP-benchmark", 0);
+	float real_time, proc_time, mflops;
+	long long flpops;
+	float ireal_time, iproc_time, imflops;
+	long long iflpops;
+	int retval;
+
+	if((retval = PAPI_flops_rate(PAPI_FP_OPS, &ireal_time, &iproc_time, &iflpops, &imflops)) < PAPI_OK)
+	{
+		printf("Error : %s", PAPI_strerror(retval));
+		printf("Could not initialise PAPI_flops \n");
+		printf("Your platform may not support floating point operation event.\n");
+		printf("retval: %d\n", retval);
+		exit(1);
+	}
 
 	kernel_doitgen_no_blocking(nr, nq, np, a_in, a_out, c4, sum);
 
+	if ((retval = PAPI_flops_rate(PAPI_FP_OPS, &real_time, &proc_time, &flpops, &mflops)) < PAPI_OK)
+	{
+		printf("retval: %d\n", retval);
+		exit(1);
+	}
+	
+	printf("Real_time: %f Proc_time: %f flpops: %lld MFLOPS: %f\n",
+		real_time, proc_time, flpops, mflops);
 
-	LSB_Finalize();
 	cleanup(a_in);
 	cleanup(a_out);
 	cleanup(c4);
