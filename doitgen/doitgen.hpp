962d0f77869222c0ace0c795494fa2bf349b0718
GrapixLeGrand <misterbibiche@gmail.com>
2021-12-02 14:44:47 +0100

    created a benchmark

--- doitgen/doitgen.hpp
+++ doitgen/doitgen.hpp
@@ -3,78 +3,6 @@
 #include <stdint.h>
 #include <mpi.h>
 
-/**
- * doitgen.h: This file is part of the PolyBench/C 3.2 test suite.
- *
- *
- * Contact: Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
- * Web address: http://polybench.sourceforge.net
- */
-/*
-#define CUSTOM_DATASET
-
-#include <../../polybench.hpp>
-#include <stdint.h>
-
-#ifndef DOITGEN_H
-# define DOITGEN_H
-
-
-# if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET)
-#  define STANDARD_DATASET
-# endif
-
-
-# if !defined(NQ) && !defined(NR) && !defined(NP)
-
-#  ifdef MINI_DATASET
-#   define NQ 10
-#   define NR 10
-#   define NP 10
-#  endif
-
-#  ifdef SMALL_DATASET
-#   define NQ 32
-#   define NR 32
-#   define NP 32
-#  endif
-
-#  ifdef STANDARD_DATASET
-#   define NQ 128
-#   define NR 128
-#   define NP 128
-#  endif
-
-#  ifdef LARGE_DATASET
-#   define NQ 256
-#   define NR 256
-#   define NP 256
-#  endif
-
-#  ifdef EXTRALARGE_DATASET
-#   define NQ 1000
-#   define NR 1000
-#   define NP 1000
-#  endif
-
-#  ifdef CUSTOM_DATASET
-#   define NQ 512
-#   define NR 512
-#   define NP 512
-#  endif
-# endif
-
-# define _PB_NQ POLYBENCH_LOOP_BOUND(NQ,nq)
-# define _PB_NR POLYBENCH_LOOP_BOUND(NR,nr)
-# define _PB_NP POLYBENCH_LOOP_BOUND(NP,np)
-
-# ifndef DATA_TYPE
-#  define DATA_TYPE double
-#  define DATA_PRINTF_MODIFIER "%0.2lf "
-# endif
-
-*/
-
 #include <string>
 
 struct problem_size_t {
@@ -108,6 +36,11 @@ const static problem_size_t benchmark_size = {128, 512, 512};
 
 #define PROCESS_MESSAGE(RANK, MESSAGE) std::cout << "(" << (RANK) << ") " << (MESSAGE) << std::endl;
 #define PROCESS_MESSAGE_2(RANK, MESSAGE_1, MESSAGE_2) std::cout << "(" << (RANK) << ") " << (MESSAGE_1) << (MESSAGE_2) << std::endl;
+#define MASTER_MESSAGE_2(MESSAGE_1, MESSAGE_2) \
+	if (rank == 0) { PROCESS_MESSAGE_2(rank, MESSAGE_1, MESSAGE_2); }
+
+#define MASTER_MESSAGE(MESSAGE) \
+	if (rank == 0) {PROCESS_MESSAGE(rank, MESSAGE); }
 
 #define ARR_2D(ARRAY, Y_DIM, X, Y) (ARRAY[ (X) * (Y_DIM) + (Y) ])
 
