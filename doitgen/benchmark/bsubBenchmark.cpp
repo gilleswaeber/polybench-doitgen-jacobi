664a07a8fac19c61157d486220c419f2335c15f0
adegendt <adegendt@student.ethz.ch>
2021-12-18 23:05:14 +0100

    Improved bsub benchmark

--- doitgen/benchmark/bsubBenchmark.cpp
+++ doitgen/benchmark/bsubBenchmark.cpp
@@ -3,20 +3,32 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <liblsb.h>
+#include <utils.hpp>
 
 void benchmark() {
-	const uint64_t table_size = 1024 * 1024 * 128;
+	const uint64_t table_size = 1024;
 	const uint64_t iter = 128;
-	double* a = (double*)malloc(table_size * sizeof(double));
+	const uint64_t max_mem = 256 * 1024 * 1024;
 	double temp = 0.0;
-
-	for (uint64_t r = 0; r < iter; ++r)  {
-		for (uint64_t i = 0; i < table_size; ++i) {
-			temp += a[i];
+	uint64_t cur_rec = 0;
+	for (uint64_t cur_size = table_size; cur_size <= max_mem; cur_size = cur_size << 1) {
+		double* a = (double*)malloc(cur_size * sizeof(double));
+		flush_cache();
+		LSB_Set_Rparam_long("TABLE_SIZE", cur_size);
+		cur_rec = 0;
+		for (uint64_t r = 0; r < iter; ++r) {
+			LSB_Res();
+			for (uint64_t i = 0; i < table_size; ++i) {
+				temp += a[i];
+			}
+			LSB_Rec(cur_rec);
 		}
+		cur_rec++;
+		free(a);
 	}
+	//Attempt at preventing optimizations
 	std::cout << "Temp : " << temp << std::endl;
-	free(a);
+	
 }
 
 int main(int argc, char** argv) {
@@ -28,7 +40,7 @@ int main(int argc, char** argv) {
 
 
 	LSB_Init("bsub_benchmark", 0);
-	LSB_Res();
+	
 	benchmark();
 	LSB_Rec(0);
 	LSB_Finalize();
